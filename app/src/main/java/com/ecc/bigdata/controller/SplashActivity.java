5f604dac3f7abee519df7b2ccd68da52227b3d8c
ManongJu <469863752@qq.com>
2017-02-23 16:42:34 +0800

    icon添加

--- app/src/main/java/com/ecc/bigdata/controller/SplashActivity.java
+++ app/src/main/java/com/ecc/bigdata/controller/SplashActivity.java
@@ -8,11 +8,14 @@ import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Arrays;
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import pub.devrel.easypermissions.AppSettingsDialog;
 import pub.devrel.easypermissions.EasyPermissions;
 
@@ -36,8 +39,10 @@ public class SplashActivity extends AppCompatActivity implements EasyPermissions
             Manifest.permission.ACCESS_FINE_LOCATION,
             Manifest.permission.INTERNET
     };
+    @BindView(R.id.welcomeTxt)
+    TextView welcomeTxt;
 
-    private Handler handler = new Handler(){
+    private Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
@@ -48,16 +53,18 @@ public class SplashActivity extends AppCompatActivity implements EasyPermissions
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_layout);
-        if (EasyPermissions.hasPermissions(this,needPermissions)){
-            Log.e(TAG,"已经获取权限");
+        ButterKnife.bind(this);
+        welcomeTxt.setText(getString(R.string.welcome_content));
+        if (EasyPermissions.hasPermissions(this, needPermissions)) {
+            Log.e(TAG, "已经获取权限");
             delayToMainActivity();
-        }else {
-            EasyPermissions.requestPermissions(this,"必要的权限",PERMISSON_REQUESTCODE,needPermissions);
+        } else {
+            EasyPermissions.requestPermissions(this, "必要的权限", PERMISSON_REQUESTCODE, needPermissions);
         }
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode,String[] permissions, int[] paramArrayOfInt) {
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] paramArrayOfInt) {
         ////把申请权限的回调交由EasyPermissions处理
         EasyPermissions.onRequestPermissionsResult(requestCode, permissions, paramArrayOfInt, this);
     }
@@ -65,7 +72,7 @@ public class SplashActivity extends AppCompatActivity implements EasyPermissions
     @Override
     public void onPermissionsGranted(int requestCode, List<String> perms) {
         Log.e(TAG, "onPermissionsGranted 获取成功的权限" + perms);
-        if (perms.containsAll(Arrays.asList(needPermissions))){
+        if (perms.containsAll(Arrays.asList(needPermissions))) {
             Log.e(TAG, "获取全部权限去主页面");
             delayToMainActivity();
         }
@@ -80,16 +87,17 @@ public class SplashActivity extends AppCompatActivity implements EasyPermissions
                     .build().show();
         }
     }
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        Log.e(TAG,"onActivityResult requestCode = "+requestCode);
+        Log.e(TAG, "onActivityResult requestCode = " + requestCode);
         if (requestCode == AppSettingsDialog.DEFAULT_SETTINGS_REQ_CODE) {
-            if (EasyPermissions.hasPermissions(this,needPermissions)){
+            if (EasyPermissions.hasPermissions(this, needPermissions)) {
                 Toast.makeText(this, "应用已经取得相应的权限，马上进入主界面", Toast.LENGTH_SHORT)
                         .show();
                 delayToMainActivity();
-            }else{
+            } else {
                 Toast.makeText(this, "应用得不到相应的权限，即将退出", Toast.LENGTH_SHORT)
                         .show();
                 handler.postDelayed(new Runnable() {
@@ -97,19 +105,20 @@ public class SplashActivity extends AppCompatActivity implements EasyPermissions
                     public void run() {
                         finish();
                     }
-                },2000);
+                }, 2000);
             }
         }
     }
-    private void delayToMainActivity(){
+
+    private void delayToMainActivity() {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
                 startActivity(intent);
                 finish();
             }
-        },3000);
+        }, 3000);
     }
 
     @Override
